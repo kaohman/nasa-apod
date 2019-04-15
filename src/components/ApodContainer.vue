35b1abc850f29ada17fbd103d66865054526ab0a
Karin Ohman <kaohma@gmail.com>
2019-04-15 14:17:35 -0600

    Add initial component setup

---
+++ src/components/ApodContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="apod-container">
+  </div>
+</template>
+
+<script>
+import Apod from './Apod';
+import { fetchData } from '../utils/api.js';
+
+export default {
+  name: 'ApodContainer',
+
+  components: {
+    Apod
+  },
+
+  mounted: function() {
+    this.getImage()
+  },
+
+  methods: {
+    getImage: async function() {
+      // await fetchData();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

184b38004101bfd1a251b2034fdd284a67a8f705
Karin Ohman <kaohma@gmail.com>
2019-04-16 14:20:32 -0600

    Update env variable

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 export const fetchData = async (params = '') => {
-  let url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
+  let url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.VUE_APP_API_KEY}`;
 
   if (params.length) {
     url = url + '&date=' + params;
   }
-  
+
   const response = await fetch(url);
   const json = await response.json();
 

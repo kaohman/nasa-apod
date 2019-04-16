cb91a8960194a6bd146bdad350585e9cc7171764
Karin Ohman <kaohma@gmail.com>
2019-04-16 13:49:34 -0600

    Update server and package.json for production

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const path = require('path');
 
 let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname + "/dist")));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
